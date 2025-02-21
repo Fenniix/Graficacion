733e087d9ec71223bc2c6aaaab5511ef7d2c2ad5
Fenniix <157856142+Fenniix@users.noreply.github.com>
2025-02-20 23:17:45 -0600

    Add files via upload

---
+++ Graficadora/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace Graficadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btndibujar = new System.Windows.Forms.Button();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.txtY2 = new System.Windows.Forms.TextBox();
+            this.txtY1 = new System.Windows.Forms.TextBox();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtX3 = new System.Windows.Forms.TextBox();
+            this.txtY3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btndibujar
+            // 
+            this.btndibujar.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.btndibujar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btndibujar.Location = new System.Drawing.Point(63, 282);
+            this.btndibujar.Name = "btndibujar";
+            this.btndibujar.Size = new System.Drawing.Size(75, 23);
+            this.btndibujar.TabIndex = 0;
+            this.btndibujar.Text = "Dibujar";
+            this.btndibujar.UseVisualStyleBackColor = false;
+            this.btndibujar.Click += new System.EventHandler(this.btndibujar_Click);
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(46, 93);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(39, 20);
+            this.txtX1.TabIndex = 1;
+            // 
+            // txtY2
+            // 
+            this.txtY2.Location = new System.Drawing.Point(126, 167);
+            this.txtY2.Name = "txtY2";
+            this.txtY2.Size = new System.Drawing.Size(39, 20);
+            this.txtY2.TabIndex = 2;
+            // 
+            // txtY1
+            // 
+            this.txtY1.Location = new System.Drawing.Point(126, 93);
+            this.txtY1.Name = "txtY1";
+            this.txtY1.Size = new System.Drawing.Size(39, 20);
+            this.txtY1.TabIndex = 3;
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(46, 167);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(39, 20);
+            this.txtX2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ingresa los valores";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(106, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Y";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 248);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "X";
+            // 
+            // txtX3
+            // 
+            this.txtX3.Location = new System.Drawing.Point(46, 241);
+            this.txtX3.Name = "txtX3";
+            this.txtX3.Size = new System.Drawing.Size(39, 20);
+            this.txtX3.TabIndex = 11;
+            // 
+            // txtY3
+            // 
+            this.txtY3.Location = new System.Drawing.Point(126, 241);
+            this.txtY3.Name = "txtY3";
+            this.txtY3.Size = new System.Drawing.Size(39, 20);
+            this.txtY3.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(74, 64);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Valor de A";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(74, 140);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Valor de B";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(74, 214);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Valor de C";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(401, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 350);
+            this.panel1.TabIndex = 17;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(184, 96);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(55, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Punto rojo";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.Color.Blue;
+            this.label12.Location = new System.Drawing.Point(184, 170);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(57, 13);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Punto azul";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.ForeColor = System.Drawing.Color.Green;
+            this.label13.Location = new System.Drawing.Point(183, 244);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(65, 13);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "Punto verde";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtX3);
+            this.Controls.Add(this.txtY3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtX2);
+            this.Controls.Add(this.txtY1);
+            this.Controls.Add(this.txtY2);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.btndibujar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btndibujar;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.TextBox txtY2;
+        private System.Windows.Forms.TextBox txtY1;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtX3;
+        private System.Windows.Forms.TextBox txtY3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Graficadora/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graficadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btndibujar_Click(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Update();
+
+            // Obtener las coordenadas de los TextBox
+            if (int.TryParse(txtX1.Text, out int x1) && int.TryParse(txtY1.Text, out int y1) &&
+                int.TryParse(txtX2.Text, out int x2) && int.TryParse(txtY2.Text, out int y2) &&
+                int.TryParse(txtX3.Text, out int x3) && int.TryParse(txtY3.Text, out int y3))
+            {
+                // Calcular el tamaño del panel
+                int minX = Math.Min(Math.Min(x1, x2), x3);
+                int minY = Math.Min(Math.Min(y1, y2), y3);
+                int maxX = Math.Max(Math.Max(x1, x2), x3);
+                int maxY = Math.Max(Math.Max(y1, y2), y3);
+
+                // Ajustar el tamaño del panel
+                int padding = 5; // Espacio adicional para el borde
+                int newWidth = maxX - minX + padding;
+                int newHeight = maxY - minY + padding;
+
+                // Establecer un tamaño mínimo para el panel
+                int minWidth = 400; // Ancho mínimo
+                int minHeight = 350; // Alto mínimo
+
+                panel1.Width = Math.Max(newWidth, minWidth);
+                panel1.Height = Math.Max(newHeight, minHeight);
+
+                // Mantener el panel en la parte inferior derecha
+                panel1.Location = new Point(this.ClientSize.Width - panel1.Width - 10, this.ClientSize.Height - panel1.Height - 10);
+            }
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+                // Obtener las coordenadas de los TextBox
+            if (int.TryParse(txtX1.Text, out int x1) && int.TryParse(txtY1.Text, out int y1) &&
+                int.TryParse(txtX2.Text, out int x2) && int.TryParse(txtY2.Text, out int y2)) //&&
+                //int.TryParse(txtX3.Text, out int x3) && int.TryParse(txtY3.Text, out int y3))
+            {
+                // Invertir las coordenadas Y
+                int invertedY1 = panel1.Height - y1;
+                int invertedY2 = panel1.Height - y2;
+                //int invertedY3 = panel1.Height - y3;
+
+                // Dibujar los puntos
+                e.Graphics.FillEllipse(Brushes.Red, x1 - 5, y1 - 5, 10, 10); // Punto A
+                e.Graphics.FillEllipse(Brushes.Blue, x2 - 5, y2 - 5, 10, 10); // Punto B
+                //e.Graphics.FillEllipse(Brushes.Green, x3 - 5, y3 - 5, 10, 10); // Punto C
+
+                // Dibujar una línea entre los puntos
+                e.Graphics.DrawLine(Pens.White, x1, y1, x2, y2); // Línea A-B
+                //e.Graphics.DrawLine(Pens.White, x2, y2, x3, y3); // Línea B-C
+                //e.Graphics.DrawLine(Pens.White, x3, y3, x1, y1); // Línea C-A
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese coordenadas válidas.");
+            }
+        }
+    }
+}
